9a70ca90d42c23234902abdd148dcb2986847ba9
Marlon Branco <marlonaparecido18@gmail.com>
2021-02-18 18:23:37 -0300

    chore: Testes dos recursos iniciais completos

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,45 @@
+import { hash } from 'bcryptjs';
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import User from '../infra/typeorm/entities/User';
+import IHashProvider from '../providers/HashProvider/modules/IHashProvider';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+@injectable()
+class CreateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const checkUserExists = await this.usersRepository.findByEmail(email);
+
+    if (checkUserExists) {
+      throw new AppError('Email adress already used.');
+    }
+
+    const hashedPassword = await this.hashProvider.generateHash(password);
+
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return user;
+  }
+}
+
+export default CreateUserService;
